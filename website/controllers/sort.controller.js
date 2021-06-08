caeb7ff87eb43c8d338339b82e6fa1c747898eb3
TUAN\Tuan <tuannguyentn2504@gmail.com>
2021-06-08 17:07:16 +0700

    Fix menu & slide menu

--- website/controllers/sort.controller.js
+++ website/controllers/sort.controller.js
@@ -1,11 +1,21 @@
 exports.getSortPage = (req, res, next) => {
 	try {
 		return res.render('sort', {
-			title: 'Dyno - Sorting Algorithms',
+			title: 'Dyno - Sorting Visualizer',
 			current: 'algorithm',
 			sub: 'sort',
 		});
 	} catch (error) {
 		console.error('GET SORT PAGE ERROR: ', error);
 	}
+};
+
+exports.getSortRealtimePage = (req, res, next) => {
+	try {
+		return res.render('sort-realtime', {
+			title: 'Dyno - Sorting Realtime',
+			current: 'algorithm',
+			sub: 'sort-realtime',
+		});
+	} catch (error) {}
 };
--- website/routes/sort.route.js
+++ website/routes/sort.route.js
@@ -2,6 +2,7 @@ const sortRoute = require('express').Router();
 const sortController = require('../controllers/sort.controller');
 
 // get sort algorithm page
-sortRoute.get('/', sortController.getSortPage);
+sortRoute.get('/visualizer', sortController.getSortPage);
+sortRoute.get('/realtime', sortController.getSortRealtimePage);
 
 module.exports = sortRoute;
