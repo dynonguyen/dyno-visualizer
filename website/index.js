a550b259b3966a1cd882009d4e9ade71cc4cf363
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-07-17 14:48:59 +0700

    ⚒ Remove fontawesome change to cnd

--- website/index.js
+++ website/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const path = require('path');
 const homeRoute = require('./routes/home.route');
-const sortRoute = require('./routes/sort.route')
+const sortRoute = require('./routes/sort.route');
+const https = require('https');
 
 // set port
 const PORT = process.env.PORT || 3333;
@@ -16,12 +16,23 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 // set up cookie, body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use('/', homeRoute);
 app.use('/algorithm/sort', sortRoute);
+
+// Auto wake up heroku
+/* 
+
+app.get('/apis/wakeup-heroku', (req, res) => res.send('ok'));
+const timer = 10 * 60 * 1000; // 10 minutes
+setInterval(() => {
+	https.get('https://dyno-visualizer.herokuapp.com/apis/wakeup-heroku');
+}, timer);
+
+*/
 
 // =============== LISTENING ================= //
 app.listen(PORT, () => {
