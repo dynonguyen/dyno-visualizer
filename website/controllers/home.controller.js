b41489a240327fcf98f13ea017ce6490e7b723e2
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-07-17 18:09:30 +0700

    🛠  Convert Dynamic website to static website

--- website/controllers/home.controller.js
+++
@@ -1,9 +0,0 @@
-exports.getHome = (req, res, next) => {
-	try {
-		return res.render('index', {
-			current: 'home',
-		});
-	} catch (error) {
-		console.error('GET HOME ERROR: ', error);
-	}
-};
--- website/routes/sort.route.js
+++
@@ -1,8 +0,0 @@
-const sortRoute = require('express').Router();
-const sortController = require('../controllers/sort.controller');
-
-// get sort algorithm page
-sortRoute.get('/visualizer', sortController.getSortPage);
-sortRoute.get('/realtime', sortController.getSortRealtimePage);
-
-module.exports = sortRoute;
